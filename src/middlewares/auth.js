754d965fc97632e8a937fa8764739f5b09d75e55
smraddhi <smraddhir@fluxlabs.in>
2025-05-10 16:56:42 +0530

    modified

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,7 +9,7 @@ const auth = async (req, _res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
     token = req.headers.authorization.split(' ')[1]; 
   } else {
-    throw AppError('Not authorized, no token',401,);
+    throw new AppError('Not authorized, no token',401,);
   }
 
   try {
@@ -18,13 +18,13 @@ const auth = async (req, _res, next) => {
     req.user = await User.findById(decoded.id).select('-password');
     console.log(`Decoded user ID: ${decoded.id}`);
     if (!req.user) {
-      throw AppError('Not authorized, user not found',401); 
+      throw new AppError('Not authorized, user not found',401); 
     }
 
     next(); 
   } catch (error) {
     console.error('Error in authentication middleware:', error);
-    throw AppError('Not authorized, token failed',401); 
+    throw new AppError('Not authorized, token failed',401); 
   }
 };
 
